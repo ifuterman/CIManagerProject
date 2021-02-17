20cc7fdfba094e79d68aafe087979a95d9cb87a5
Iosif Futerman <ifuterman@yandex.ru>
2021-02-17 13:43:25 +0500

    some refactoring after asshole bursting

--- cim_server/lib/controllers/src/debug_clean_db_controller.dart
+++ cim_server/lib/controllers/src/debug_clean_db_controller.dart
@@ -5,16 +5,18 @@ import 'package:cim_server/model/cim_user_db.dart';
 
 class DebugCleanDBController extends Controller{
   DebugCleanDBController(this.context);
-  ManagedContext context;
+  final ManagedContext context;
 
   @override
   FutureOr<RequestOrResponse> handle(Request request) async{
     try{
       final tokenQuery = Query<CIMToken>(context);
+      tokenQuery.canModifyAllInstances = true;
       await tokenQuery.delete();
       final userQuery = Query<CIMUserDB>(context);
+      userQuery.canModifyAllInstances = true;
       await userQuery.delete();
-      Response.ok({"message" : "Users, Tokens is deleted"});
+      return Response.ok({"message" : "Users, Tokens is deleted"});
     }catch(e){
       return Response.serverError(body: {"message" : e.toString()});
     }
