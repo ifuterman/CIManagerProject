143c333433118b75134468aba5752feceec4267d
Iosif Futerman <ifuterman@yandex.ru>
2021-03-02 11:55:32 +0500

    doctor functionality

--- cim_server/lib/controllers/src/doctor_update_controller.dart
+++ cim_server/lib/controllers/src/doctor_update_controller.dart
@@ -2,8 +2,8 @@ import 'package:cim_server/cim_server.dart';
 import 'package:cim_protocol/cim_protocol.dart';
 import 'package:cim_server/model/cim_doctor_db.dart';
 
-class DoctorDeleteController extends Controller{
-  DoctorDeleteController(this.context);
+class DoctorUpdateController extends Controller{
+  DoctorUpdateController(this.context);
   final ManagedContext context;
 
   @override
@@ -17,12 +17,23 @@ class DoctorDeleteController extends Controller{
       }
       var doctor = list[0] as CIMDoctor;
       final query = Query<CIMDoctorDB>(context)
+        ..values.speciality = doctor.speciality
+        ..values.name = doctor.name
+        ..values.last_name = doctor.lastName
+        ..values.middle_name = doctor.middleName
+        ..values.email = doctor.email
+        ..values.birth_date = doctor.birthDate
+        ..values.phones = doctor.phones
+        ..values.users_id = doctor.userId
         ..where((x) => x.id).equalTo(doctor.id);
-      final int count = await query.delete();
-      if(count == 0){
+      final doctorDB = await query.updateOne();
+      if(doctorDB == null){
         return Response.notFound();
       }
-      return Response.ok({'message' : 'deleted'});
+      doctor = doctorDB.toDoctor();
+      packet = CIMPacket.makePacket();
+      packet.addInstance(doctor);
+      return Response.ok(packet.map);
     }catch(e){
       return Response.serverError(body: {'message' : e});
     }
