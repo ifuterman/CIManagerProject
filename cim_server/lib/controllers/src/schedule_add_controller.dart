76c96336aee39c10b4ee34b65464907152dc6980
Iosif Futerman <ifuterman@yandex.ru>
2021-03-12 14:37:07 +0500

    schedule_add_controller.dart continued. NOT COMPLETED!!!

--- cim_server/lib/controllers/src/schedule_add_controller.dart
+++ cim_server/lib/controllers/src/schedule_add_controller.dart
@@ -1,5 +1,6 @@
 import 'package:cim_server/cim_server.dart';
 import 'package:cim_protocol/cim_protocol.dart';
+import 'package:cim_server/model/cim_doctor_db.dart';
 import 'package:cim_server/model/cim_schedule_db.dart';
 
 class ScheduleAddController extends Controller{
@@ -16,6 +17,22 @@ class ScheduleAddController extends Controller{
         return Response.badRequest(body: request.body.as());
       }
       var schedule = list[0] as CIMSchedule;
+      var doctor;
+      if(schedule.doctor != null) {
+        final queryDoctor = Query<CIMDoctorDB>(context)
+          ..where((x) => x.id == schedule.doctor.id);
+        final doctorDB = await queryDoctor.fetchOne();
+        if(doctorDB == null) {
+          packet = CIMPacket.makePacket();
+          return Response(HttpStatus.expectationFailed, {}, request.body.as());
+        }
+        doctor = doctorDB.toDoctor();
+        if(doctor != schedule.doctor) {
+          return Response(HttpStatus.expectationFailed, {}, request.body.as());
+        }
+      }
+      var patient;
+
       final query = Query<CIMScheduleDB>(context)
         ..values.id = schedule.id
         ..values.duration = schedule.duration
@@ -23,10 +40,11 @@ class ScheduleAddController extends Controller{
         ..values.note = schedule.note
         ..values.doctor_id = schedule.doctor.id
         ..values.patient_id = schedule.patient.id;
-      final scheduleDB = query.insert();
+      final scheduleDB = await query.insert();
       if(scheduleDB == null){
         return Response.forbidden();
       }
+
     }catch(e){
       return Response.serverError(body: {'message' : e.toString()});
     }
