b1a7e861aeb020c5b66b8532dfb9899793275600
vvk-home <frostyland@gmail.com>
2021-05-02 09:23:02 +0500

    code: немного локализации

--- cim_client2/lib/apps/showroom/src/show_room_view.dart
+++ cim_client2/lib/apps/showroom/src/show_room_view.dart
@@ -1,6 +1,7 @@
 import 'package:cim_client2/core/getx_helpers.dart';
 import 'package:cim_client2/core/styles/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:cim_client2/core/extensions.dart';
 
 import 'show_room_view_controller.dart' as ctrl;
@@ -19,13 +20,19 @@ class ShowRoomView extends AppGetView<ctrl.ShowRoomViewController>  {
               ElevatedButton.icon(
                 onPressed: c.splashState,
                 icon: ctrl.State.splash.icon,
-                label: Text('splash'),
+                label: Text('to_splash'.tr()),
               ),
               10.h(),
               ElevatedButton.icon(
                 onPressed: c.homeState,
                 icon: ctrl.State.home.icon,
-                label: Text('home'),
+                label: Text('to_home'.tr()),
+              ),
+              10.h(),
+              ElevatedButton.icon(
+                onPressed: c.homeState,
+                icon: ctrl.State.home.icon,
+                label: Text('to_excel'.tr()),
               ),
             ],
           ),
